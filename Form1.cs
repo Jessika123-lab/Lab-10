6f1fe821b676ac97d872e566ec3e81a3217b3cde
Jessika123-lab <73876428+Jessika123-lab@users.noreply.github.com>
2021-05-25 23:13:56 +0300

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab10_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a1 = (int)numericUpDown1.Value;
+            int d = (int)numericUpDown2.Value;
+            int n = (int)numericUpDown3.Value;
+
+            int sum = 0;
+
+            for (int i = 0; i < numericUpDown3.Value; i++)
+            {
+                sum = ((2*a1) + d*(n-1))/2 * n;
+            }
+
+            MessageBox.Show("" + sum);
+        }
+    }
+}
